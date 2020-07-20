70d02e427a0c7b643e0741cb510a4ae2303c3f06
souvick C <souvickdesign1@gmail.com>
2020-07-20 19:46:40 -0400

    remove gitignore files

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
+})
+export class PostCreateComponent {
+  newPost = '';
+  enteredValue = '';
+
+  onAddPost() {
+    // console.dir(postInput);
+    // this.newPost = postInput.value;
+    this.newPost = this.enteredValue;
+  }
+}
